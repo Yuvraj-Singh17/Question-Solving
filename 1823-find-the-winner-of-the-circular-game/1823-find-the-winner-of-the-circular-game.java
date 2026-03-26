06b634448718cfed97b78b50c4514aea8f800fd2
Yuvraj-Singh17 <yuvivishu17@gmail.com>
2026-03-26 22:48:03 +0530

    Time: 0 ms (100%), Space: 42.3 MB (64.32%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,28 +1,11 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        boolean[] arr = new boolean[n];
-        return winner(arr , n , k , 0 , n);
+        return winner(n,k)+1;
     }
-    public int winner(boolean[] person , int n , int k , int idx , int personleft){
-        if(personleft == 1){
-            for(int i = 0 ;  i < n ; i++){
-                if(person[i] == false){
-                    return i+1;
-                }
-            }
+    public int winner(int n , int k){
+        if(n == 1){
+            return 0;
         }
-        int kill = (k-1)%n;
-        while(kill-- > 0){
-            idx = (idx+1)%n;
-            while(person[idx] == true){
-                idx = (idx + 1)%n;
-            }
-        }
-        person[idx] = true;
-
-        while(person[idx] == true){
-            idx = (idx+1)%n;
-        }
-        return winner(person , n , k , idx , personleft-1);
+        return (winner(n-1, k)+k)%n;
     }
 }
