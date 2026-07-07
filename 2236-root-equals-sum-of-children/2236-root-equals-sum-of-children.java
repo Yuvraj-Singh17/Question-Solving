fd11c11a11309cc183797574b5da66976a52f8d1
Yuvraj-Singh17 <yuvivishu17@gmail.com>
2026-07-08 00:59:53 +0530

    Time: 0 ms (100%), Space: 43.3 MB (13.48%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -15,6 +15,12 @@
  */
 class Solution {
     public boolean checkTree(TreeNode root) {
-        
+        if(root == null){
+            return false;
+        }
+        if(root.val == (root.left.val + root.right.val)){
+            return true;
+        }
+        return false;
     }
 }
