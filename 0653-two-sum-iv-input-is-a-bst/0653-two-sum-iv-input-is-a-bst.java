48e38a9b7357172f406be20703cdfa09f2f47bb2
Yuvraj-Singh17 <yuvivishu17@gmail.com>
2026-07-23 09:30:30 +0530

    Time: 3 ms (74.23%), Space: 47.6 MB (10.16%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -2,64 +2,29 @@
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
- *     TreeNode left, right;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-
-import java.util.*;
-
-class BSTIterator {
-    Stack<TreeNode> stack = new Stack<>();
-    boolean reverse;
-
-    public BSTIterator(TreeNode root, boolean isReverse) {
-        reverse = isReverse;
-        pushAll(root);
-    }
-
-    private void pushAll(TreeNode node) {
-        while (node != null) {
-            stack.push(node);
-            if (reverse) node = node.right;
-            else node = node.left;
-        }
-    }
-
-    public int next() {
-        TreeNode node = stack.pop();
-        
-        if (!reverse) {
-            pushAll(node.right);
-        } else {
-            pushAll(node.left);
-        }
-
-        return node.val;
-    }
-}
-
 class Solution {
+
+    HashSet<Integer> set = new HashSet<>();
+
     public boolean findTarget(TreeNode root, int k) {
-        if (root == null) return false;
+        if(root == null) return false;
 
-        BSTIterator left = new BSTIterator(root, false); 
-        BSTIterator right = new BSTIterator(root, true); 
-
-        int i = left.next();
-        int j = right.next();
-
-        while (i < j) {
-            int sum = i + j;
-
-            if (sum == k) return true;
-            else if (sum < k) {
-                i = left.next();
-            } else {
-                j = right.next();
-            }
+        if(set.contains(k - root.val)){
+            return true;
         }
+        set.add(root.val);
 
-        return false;
+        return findTarget(root.left , k) || findTarget(root.right , k);
     }
 }
