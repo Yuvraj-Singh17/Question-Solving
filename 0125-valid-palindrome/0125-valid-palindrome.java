1427a532a13584f2434a02f580ffc12ed5fbc847
Yuvraj-Singh17 <yuvivishu17@gmail.com>
2026-08-03 00:17:53 +0530

    Time: 7 ms (35.13%), Space: 44.8 MB (44.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();  // Convert the string to lowercase
+        s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
+
+        for(int i = 0 ; i < s.length() ; i++){
             char c = s.charAt(i);
             if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
                 sb.append(c);
             }
         }
+        System.out.println(sb);
+        
+
         int i = 0;
-        int j = sb.length() - 1;
-        while (i <= j) {
-            if (sb.charAt(i) != sb.charAt(j)) {
-                return false;
-            }
+        int j = sb.length()-1;
+
+        while(i <= j){
+            if(sb.charAt(i) != sb.charAt(j)) return false;
             i++;
             j--;
         }
-        
         return true;
     }
 }
